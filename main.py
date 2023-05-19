6a2d05b7e62d1f7c593eb8ebd8e3e6972586f220
zolbayar damba <zolbayardamba@zolbayars-MacBook-Air.local>
2023-05-18 21:19:17 -0500

    added new files

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/zoloo')
+def hello_zoloo():
+    return 'Hello, Zoloo!'
+
+@app.route('/form', methods=['GET', 'POST'])
+def render_form():
+    message = ''
+    if request.method == 'POST':
+        text = request.form.get('text')
+        message = text.lower()
+    return render_template('form.html', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
